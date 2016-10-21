de6141a3cee2c5d16e99b45a1a83822f6309ccee
louisbl <louis@beltramo.me>
2016-10-21 17:11:31 +0200

    Add Sugar ORM. Need to disable instant run

--- app/src/main/java/ddi/gobelins/mynotes/datas/Note.java
+++ app/src/main/java/ddi/gobelins/mynotes/datas/Note.java
@@ -1,10 +1,12 @@
 package ddi.gobelins.mynotes.datas;
 
+import com.orm.SugarRecord;
+
 /**
  * Created by lbeltramo on 20/10/2016.
  */
 
-public class Note {
+public class Note extends SugarRecord {
     public String title;
     public String description;
 
